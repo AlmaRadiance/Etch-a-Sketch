7c8cea2bf5d3f7f474f1ef1b563c62fab3ad5736
Alma <codingskills198@gmail.com>
2023-10-21 13:44:45 -0400

    Transfer CSS styles to JavaScript for dynamic behavior

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ header.classList.add('header');
 const h1=document.createElement('h1');
 h1.textContent='Etch-a-Sketch';
 header.appendChild(h1);
+header.style.position="static";
+header.style.bottom="0";
+header.style.left="0";
+header.style.width="100%";
+header.style.padding="10px";
+header.style.marginBottom="20px";
+header.style.background="rgba(83, 1, 46, 0.9)";
+
+h1.style.color="white";
+h1.style.fontFamily="Copperplate, sans-serif";
+h1.style.display="block";
+h1.style.fontSize="32px";
+h1.style.padding="10px";
+h1.style.textAlign="center";
+h1.style.margin="10px";
 
 //Body (Drawing Area)
 const body=document.createElement('div');
@@ -30,6 +45,16 @@ newDiv.style.marginTop = "70px";
 const footer=document.createElement('footer');
 footer.classList.add('footer');
 footer.textContent = '\u00A9 2023 Etch-a-Sketch - The Odin Project | By: Alma P.';
+footer.style.position="fixed";
+footer.style.bottom="0";
+footer.style.left="0";
+footer.style.width="100%";
+footer.style.textAlign="center";
+footer.style.padding="1rem";
+footer.style.paddingBottom="1px";
+footer.style.background="rgba(83, 1, 46, 0.9)";
+footer.style.color="#fff";
+footer.style.fontFamily="Copperplate,sans-serif";
 
 // Append elements to the body
 document.body.appendChild(header);
